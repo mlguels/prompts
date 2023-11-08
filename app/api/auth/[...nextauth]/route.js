fca9dff17edbe8e510d289afd648c43e80445569
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-07 23:06:46 -0800

    Fixed Bug: Needed callback

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,36 +11,37 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
 
-    session.user.id = sessionUser._id;
+      session.user.id = sessionUser._id;
 
-    return session;
-  },
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+        // Check if user exists in database
+        const userExists = await User.findOne({ email: profile.email });
 
-      // Check if user exists in database
-      const userExists = await User.findOne({ email: profile.email });
-
-      // If user does not exist, create them in the database and then sign them in
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        // If user does not exist, create them in the database and then sign them in
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
