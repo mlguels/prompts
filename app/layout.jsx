d8741394dced51da45b75b70d9602baf4ed298bf
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-14 17:40:25 -0800

    Refactoring Project

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,7 @@
-import "@styles/globals.css";
+import React from "react";
 
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
-
-export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
-};
-
-const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
-
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
-      </body>
-    </html>
-  );
+const RootLayout = () => {
+  return <div>RootLayout</div>;
 };
 
 export default RootLayout;
