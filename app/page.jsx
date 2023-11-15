ca6c60ec676f82979ce10f920abdf4dd8a637f72
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-14 19:38:10 -0800

    Add: components

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & share
-        <br className="max-md:hidden" />
+        Discover & Share
+        <br className="max-md:hidden" />{" "}
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </h1>
 
