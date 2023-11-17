82b0cdbc05e62832d536fb46d9995d40c6870edc
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-17 08:45:37 -0800

    Deleted depricated options

--- utils/database.js
+++ utils/database.js
@@ -11,10 +11,7 @@ export const connectToDatabase = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
 
     isConnected = true;
     console.log({ "MongoDB is connected": "🎉" });
