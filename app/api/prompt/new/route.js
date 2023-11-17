5c0194b9626ac4475e00b610750037e9f4257030
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-17 08:35:40 -0800

    Add: backend api route for creating prompt

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,21 @@
+import { connectToDatabase } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const POST = async (request) => {
+  const { userId, prompt, tag } = request.json();
+
+  try {
+    await connectToDatabase();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag,
+    });
+
+    await newPrompt.save();
+
+    return new Response(JSON.stringify(newPrompt), { status: 201 });
+  } catch (error) {
+    return new Response("Failed to create prompt", { status: 500 });
+  }
+};
