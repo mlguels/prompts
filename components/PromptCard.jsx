2564d64070335b753596fddf8b213de00292aed6
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-17 22:16:50 -0800

    Updated PromptCard to accept edit and delete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,10 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -13,6 +17,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     navigator.clipboard.writeText(post.prompt);
     setTimeout(() => setCopied(""), 3000);
   };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -54,6 +59,22 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+      {/* {session?.user.id === post.creator._id && pathName === "/profile" && ( */}
+      <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+        <p
+          className="font-inter text-sm green_gradient cursor-pointer"
+          onClick={handleEdit}
+        >
+          Edit
+        </p>
+        <p
+          className="font-inter text-sm orange_gradient cursor-pointer"
+          onClick={handleDelete}
+        >
+          Delete
+        </p>
+      </div>
+      {/* )} */}
     </div>
   );
 };
