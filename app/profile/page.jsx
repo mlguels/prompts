921a9f78aed01547e7a53c18d7d6e75be3a4a04f
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-18 21:58:07 -0800

    Fixed bug

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,36 +4,13 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/Profile";
+import Profile from "@components/profile";
 
-const ProfilePage = () => {
+const MyProfile = () => {
   const router = useRouter();
   const { data: session } = useSession();
 
   const [posts, setPosts] = useState([]);
-
-  const handleEdit = (post) => {
-    router.push(`/update-prompt?id=${post._id}`);
-  };
-
-  const handleDelete = async (post) => {
-    const hasConfirmed = confirm(
-      "Are you sure you want to delete this prompt?"
-    );
-
-    if (!hasConfirmed) {
-      try {
-        await fetch(`/api/prompt/${post._id.toString()}`, {
-          method: "DELETE",
-        });
-        const filteredPosts = posts.filter((p) => p._id !== post._id);
-
-        setPosts(filteredPosts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -46,10 +23,34 @@ const ProfilePage = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <Profile
       name="My"
-      desc="Welcome to your profile page"
+      desc="Welcome to your personalized profile page"
       data={posts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
@@ -57,4 +58,4 @@ const ProfilePage = () => {
   );
 };
 
-export default ProfilePage;
+export default MyProfile;
